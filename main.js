365a9efe6848f2ffc3d5b86434ce7a460b4d1459
baek-shawn <60677948+baek-shawn@users.noreply.github.com>
2020-07-20 10:13:02 +0900

    JS:Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -11,8 +11,9 @@ document.addEventListener('scroll',() => {
     }
 });
 
+
 /Handle scrolling when tapping on the navbar menu/
-const navbarMenu=document.querySelector('.navbar__menu')
+const navbarMenu=document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event)=>{
     
     const target= event.target;
@@ -21,9 +22,17 @@ navbarMenu.addEventListener('click', (event)=>{
         return;
     }
 
-    console.log(event.target.dataset.link);
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 }); 
+
+
+/Navbar toggle button for small screen/
+const navbarToggleBtn=document.querySelector('.navbar__togle-btn');
+navbarToggleBtn.addEventListener('click', ()=>{
+    navbarMenu.classList.toggle('open');
+});
+
 
 /Handle click on "contactme" button on home/
 const homeContactBtn=document.querySelector('.home__contact');
@@ -65,20 +74,20 @@ workBtnContainer.addEventListener('click',(e)=>{
         return;
     }
 
-    /Remove selection from the previous item and select the new one/
-    const active=document.querySelector('.category__btn.selected');
-    active.classList.remove('selected');
-    const target=e.target.nodeName==='BUTTON' ? e.target : e.target.parentNode;
-    target.classList.add('selected') 
-    projectContainer.classList.add('anim-out');
-    setTimeout(()=>{
-        projects.forEach((project)=>{
-            console.log(project.dataset.type);
-            if(filter==='*'||filter===project.dataset.type){
-                project.classList.remove('invisible');
-            } else{
-                project.classList.add('invisible');
-            }
+/Remove selection from the previous item and select the new one/
+const active=document.querySelector('.category__btn.selected');
+active.classList.remove('selected');
+const target=e.target.nodeName==='BUTTON' ? e.target : e.target.parentNode;
+target.classList.add('selected') 
+projectContainer.classList.add('anim-out');
+setTimeout(()=>{
+    projects.forEach((project)=>{
+        console.log(project.dataset.type);
+        if(filter==='*'||filter===project.dataset.type){
+            project.classList.remove('invisible');
+        } else{
+            project.classList.add('invisible');
+        }
         });
         projectContainer.classList.remove('anim-out');
     }, 300);
